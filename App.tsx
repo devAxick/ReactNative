e29500cf4b0742998f4be26aa12ab37756bd05c5
DanyloAksonov <danyl.aksonov@sysgears.com>
2023-03-05 14:19:01 +0200

    Installed react-native-elements lib.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import { Card } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Card>
+        <Card.Title>Title</Card.Title>
+        <Card.Divider />
+        <Card.Image
+          source={{
+            uri: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg',
+          }}
+        />
+      </Card>
       <StatusBar style="auto" />
     </View>
   );
